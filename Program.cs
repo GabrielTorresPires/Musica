dc32333fca79b15055598e78f89079d4176f68da
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-09-09 22:33:22 -0300

    solução do exercício Música

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Como se depois de um longo dia ruim");
+Console.WriteLine("Tivesse um lindo Sonho");
+Console.WriteLine("Você surgiu e minha alma inteira sorriu");
+Console.WriteLine("Como se depois de um longo dia ruim");
+Console.WriteLine("Tivesse um lindo Sonho");
+Console.WriteLine("Você sugiu, tan sua, me trouxe, sua mão");
+Console.WriteLine("Como se depois de um longo dia ruim");
+Console.WriteLine("Tivesse um lindo Sonho");
+Console.WriteLine("Você surgiu, dizendo-me nada foi em vão");
+Console.WriteLine("Gabriel Torres");
+
+
