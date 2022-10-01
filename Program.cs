78a14fd0ad4c7b23ea050b4a4ff1f007ff51a2d0
Gabriel Torres <gabrieltlpires@gmail.com>
2022-09-30 22:34:43 -0300

    Alteração de cor e git config

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
-﻿Console.WriteLine("Como se depois de um longo dia ruim");
+﻿Console.ForegroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("Como se depois de um longo dia ruim");
 Console.WriteLine("Tivesse um lindo Sonho");
 Console.WriteLine("Você surgiu e minha alma inteira sorriu");
+Console.ResetColor();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkGreen;
 Console.WriteLine("Como se depois de um longo dia ruim");
 Console.WriteLine("Tivesse um lindo Sonho");
 Console.WriteLine("Você sugiu, tan sua, me trouxe, sua mão");
+Console.ResetColor();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkYellow;
 Console.WriteLine("Como se depois de um longo dia ruim");
 Console.WriteLine("Tivesse um lindo Sonho");
 Console.WriteLine("Você surgiu, dizendo-me nada foi em vão");
+Console.ResetColor();
+Console.WriteLine();
 Console.WriteLine("Gabriel Torres");
+
+
 
 
